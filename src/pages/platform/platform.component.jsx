01eb62b68596877d0b2fa6ba1c05e3cb831f8bd2
bdanzer <britt.danzer@gmail.com>
2020-02-09 04:46:32 -0600

    removed react-redux method from platform

--- src/pages/platform/platform.component.jsx
+++ src/pages/platform/platform.component.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
 import { useParams, useHistory } from 'react-router-dom'
 
 import Modal from '../../components/modal/modal.component'
